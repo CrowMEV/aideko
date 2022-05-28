8b4190d064ff6518e73b93e892b13109d3ee39cd
Evgeny Malikov <mev@offershub.ru>
2022-05-28 08:54:00 +0500

    add status filter

--- app/views.py
+++ app/views.py
@@ -7,18 +7,20 @@ from tools import get_ports_info
 
 
 async def ip_scaner(request):
-    print(request.text)
     syslog.syslog(syslog.LOG_INFO, f'request from {request.remote}')
     result = []
+    ip = request.query.get('ip', '127.0.0.1')
+    begin = int(request.query.get('begin', 0))
+    end = int(request.query.get('end', 65535))
+    status = request.query.get('status')
     try:
-        ip = request.query.get('ip', '127.0.0.1')
-        begin = int(request.query.get('begin', 0))
-        end = int(request.query.get('end', 65535))
         for ports in chunked(range(begin, end+1), 10):
             async for port in get_ports_info(ports, ip):
                 result.append(port)
     except ValueError as err:
         syslog.syslog(syslog.LOG_ERR, str(err))
+    if status:
+        result = [port for port in result if port['status'] == status]
     return web.json_response(result)
 
 
